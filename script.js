3cad0f6193daad54003580f046878071de741422
saeedhalabi <saeedhalabi80@gmail.com>
2023-01-11 10:58:46 +0200

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const toggle = document.getElementById('toggleDark');
+const body = document.querySelector('body');
+
+toggle.addEventListener('click', function(){
+  this.classList.toggle('fa-sun');
+  this.classList.toggle('fa-moon');
+  if(this.classList.contains('fa-sun')){
+    body.style.background = 'white';
+    body.style.color = 'black';
+    body.style.transition = '2s';
+  }
+  else{
+    body.style.background = 'black';
+    body.style.color = 'white';
+    body.style.transition = '2s';
+  }
+});
